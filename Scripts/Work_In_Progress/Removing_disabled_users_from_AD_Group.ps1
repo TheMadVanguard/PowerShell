26825af8b334f855764bdda6c770aa37d0eb4c97
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-04-04 08:58:24 +0100

    Create Removing_disabled_users_from_AD_Group.ps1
    Imported from old repo, cleaned up the info section

---
+++ Scripts/Work_In_Progress/Removing_disabled_users_from_AD_Group.ps1
@@ -0,0 +1,12 @@
+﻿# Info
+#
+# This script remove disabled users from an AD Groups
+# Version 0.1
+# Written by Jay
+
+# Variables 
+$Group = "GROUP_NAME"
+
+$DisabledUser = Get-ADGroupMember -Identity $Group | Get-ADUser | Where-Object {$_.Enabled -eq $false}
+
+Remove-ADGroupMember -Identity "$Group" -Members $DisabledUser -Confirm:$false
