c3cf5a48587608aac390a5e844a3750f125860f6
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-04-04 08:24:49 +0100

    Update One_liners/Active_Directory/Find Use Member of.ps1
    Cleaned up and imported script from old repo.

---
+++ One_liners/Active_Directory/Find Use Member of.ps1
@@ -0,0 +1,9 @@
+﻿# See what groups a user is part of
+# Version 1.0
+# Written by Jay
+
+#Variables
+$Username = Read-Host -Prompt "Enter Username"
+
+#Command
+(Get-ADUser $Username –Properties MemberOf | Select-Object MemberOf).MemberOf
