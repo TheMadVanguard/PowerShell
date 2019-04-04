dc653499cdd888eb125c782e0e98206f4b52f7e4
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-04-04 08:57:21 +0100

    Create Import_CSV_to_AD_Group.ps1
    Imported from old repo and cleaned up the info section

---
+++ Scripts/Work_In_Progress/Import_CSV_to_AD_Group.ps1
@@ -0,0 +1,12 @@
+﻿# Info
+#
+# Script that adds members from a CSV file to a AD Group.
+#
+# Version 0.1
+# Written by Jay
+
+#Variables
+$Group = " AD GROUP"
+$CSVFile = "CSV Location"
+
+Import-CSV $CSVFile | %{Add-ADGroupMember -Identity $Group -Members $_.UserName} 
