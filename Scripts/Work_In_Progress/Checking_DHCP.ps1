99993c3a2ec1ce54c6824f1467857398e510ffd8
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-04-04 08:56:40 +0100

    Create Checking_DHCP.ps1
    Imported from old repo

---
+++ Scripts/Work_In_Progress/Checking_DHCP.ps1
@@ -0,0 +1,46 @@
+﻿# Info
+#
+# Script to report on DHCP Server free IP Addresses.
+#
+# Version 0.1
+# Written by Jay
+#
+# Change 0.2 by Jay
+# Removed the predefinded scope options
+
+#Variables
+$Computername = "COMPUTER NAME"
+$Scopes = "SCOPE"
+$Number = "20"
+
+$FromAddress = "FROM EMAIL"
+$ToAddress =  "TO EMAIL"
+$MyCreds = ConvertTo-SecureString "PASSWORD" -AsPlainText -Force
+$EmailServer = "SMTP Server"
+$EmailPort = "SMTP Port"
+
+Foreach($Scope in $Scopes)
+    {
+    $DHCPScope = Get-DhcpServerv4Scope -ComputerName $Computername -ScopeId $Scope
+    $DHCPScopeName = $DHCPScope.Name
+    $DHCPFreeIPs = Get-DhcpServerv4FreeIPAddress -ComputerName $Computername -ScopeId $Scope -NumAddress $Number
+    $DHCPIPsCount = ($DHCPFreeIPs | Measure-Object).Count
+  
+
+    If ($DHCPIPsCount -lt "$Number")
+        {
+        $DHCPError = "Yes" 
+        }
+    else
+        {
+        $DHCPError = "No" 
+        }
+        If ($DHCPError -eq "Yes")
+            {    
+            Send-MailMessage -From $FromAddress -To $ToAddress -usessl -Subject "DHCP Error $ComputerName $Scope" -Credential $MyCreds -SmtpServer $EmailServer -Port $EmailPort -body "An error, details are below.
+
+A fault has been reported on $ComputerName
+
+The DHCP Scope - Scope $Scope $DHCPScopeName and there are fewer than $Number IPs free"
+            }
+    }
