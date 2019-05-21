d79a6d6426e706a57bd8f2dd46c7b96176525591
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-05-21 14:03:01 +0100

    Update Connect to Office 365.ps1
    Updated the creds line

--- Scripts/Office_365/Connect to Office 365.ps1
+++ Scripts/Office_365/Connect to Office 365.ps1
@@ -7,7 +7,7 @@
 
 
 #Variables
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+$MyCreds = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $MyCreds -Authentication Basic -AllowRedirection
 
 Import-PSSession $Session
