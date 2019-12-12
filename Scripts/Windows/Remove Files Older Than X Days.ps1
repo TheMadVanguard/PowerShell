889106e4c9a4b113c6ca2c17e34d4cefc405c4ef
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-12-12 08:25:39 +0000

    Create Remove Files Older Than X Days.ps1

---
+++ Scripts/Windows/Remove Files Older Than X Days.ps1
@@ -0,0 +1,13 @@
+﻿# Info
+#
+# Script to remove files older than 15 Days
+#
+# Version 1.0
+# Written by Jay
+
+# Variables 
+$Limit = (Get-Date).AddDays(-15)
+$Path = "FOLDER LOCATION"
+
+# Delete files older than the $Limit.
+Get-ChildItem -Path $Path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $Limit } | Remove-Item -Force
