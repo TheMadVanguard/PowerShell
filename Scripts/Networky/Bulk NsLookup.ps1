26048bb2dab63f4e7fae082d694aeedc22f6b717
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-12-30 13:30:21 +0000

    Create Bulk NsLookup.ps1

---
+++ Scripts/Networky/Bulk NsLookup.ps1
@@ -0,0 +1,23 @@
+﻿# Info 
+#
+# Bulk nslookup queries.
+#
+# Version 1.0
+# Written by Jay
+
+#Variables 
+$ips = GC C:\temp\nslookup.txt
+
+
+Foreach ($ip in $ips)	{
+$name = nslookup $ip 2> $null | select-string -pattern "Name:"
+	if ( ! $name ) { $name = "" }
+	$name = $name.ToString()
+	if ($name.StartsWith("Name:")) {
+	$name = (($name -Split ":")[1]).Trim()
+	}
+	else {
+	$name = "NOT FOUND"
+	}
+Echo "$ip `t $name"
+}
