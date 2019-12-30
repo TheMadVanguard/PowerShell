e0c00d878474a3f0d228f72665f86126ab33d68f
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2019-12-30 15:58:11 +0000

    Create Credentials_Generator.ps1

---
+++ Scripts/Misc/Credentials_Generator.ps1
@@ -0,0 +1,17 @@
+﻿# Info
+#
+# This script generates a secure credentials xml
+#
+# Version 1.0
+# Written by Jay
+
+$Name = Read-Host -Prompt "Who's credentials are these?"
+$Server = $env:COMPUTERNAME
+$credName = "$Server $Name"
+Write-Host "Your credentials will be saved as $credName.xml"
+read-host -Prompt "press enter to continue"
+cls
+$ExportPath = "S:\creds.xml"
+$ExportPath
+read-host -Prompt "press enter to continue"
+$MyCredentials = Get-Credential | Export-Clixml $ExportPath 
