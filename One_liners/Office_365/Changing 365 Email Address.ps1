f9c3e02448dd3c528642337d33cc9346017664b4
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2020-03-06 07:42:01 +0000

    Created two new scripts
    1) Changing a 365 group's email address
    2) Hiding a 365 group from the gal

---
+++ One_liners/Office_365/Changing 365 Email Address.ps1
@@ -0,0 +1,8 @@
+﻿# Info
+#
+# How to change the primary SMTP address of a 365 group
+#
+# Version 1.0
+# Written by Jay
+
+Set-unifiedgroup -identity  "Group Name" -primarysmtpaddress "Email Address"
---
+++ One_liners/Office_365/Hiding a 365 Group from the GAL.ps1
@@ -0,0 +1,8 @@
+﻿# Info
+#
+# Hiding a 365 Group from the GAL
+#
+# Version 1.0
+# Written by Jay
+
+Set-UnifiedGroup -Identity "GROUP NAME" -HiddenFromAddressListsEnabled $true 
