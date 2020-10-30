1b6c21c2a643bd17c5071f635ca285f9cb2d5828
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2020-10-30 16:17:28 +0000

    New Scripts
    1) Teams image cache clear
    2) Office dkim commands

---
+++ Scripts/Office_365/Teams Clear Image Cache.ps1
@@ -0,0 +1,8 @@
+﻿# Info
+#
+# Deletes Teams Image Cache
+#
+# Version 1.0
+# Written by Jay
+
+Remove-Item "%appdata%\Microsoft\Teams\Cache" -Recurse
---
+++ Scripts/Work_In_Progress/Office365_dkim.ps1
@@ -0,0 +1,4 @@
+﻿$Domain = ""
+New-DkimSigningConfig -DomainName $Domain -Enabled $true
+Get-DkimSigningConfig -Identity $Domain | Format-List Selector1CNAME, Selector2CNAME
+set-DkimSigningConfig -Identity $Domain -Enabled $true
