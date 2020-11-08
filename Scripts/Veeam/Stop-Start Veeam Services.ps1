4f16c356fffaa14a289630d2975d59dd02ea5f2b
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2020-11-08 12:12:05 +0000

    Create Stop-Start Veeam Services.ps1

---
+++ Scripts/Veeam/Stop-Start Veeam Services.ps1
@@ -0,0 +1,37 @@
+﻿# Info
+#
+# This script stops or stats the Veeam services. 
+#
+# Version 1.0
+# Written by Jay
+
+# Gets Services
+$VeeamServices = Get-Service | Where-Object  { $_.Name -like "Veeam*" -and $_.StartType -ne "Disabled" }
+
+# Stop or Start menu
+$StopOrStartMenu = [ordered]@{
+
+  1 = 'Stop'
+
+  2 = 'Start'
+
+}
+$StopOrStartMenu1 = $StopOrStartMenu | Out-GridView -Passthru -Title 'What are you doing?'
+$StopOrStart = $StopOrStartMenu1.value
+
+# Stop or Start action
+if ($StopOrStart -eq "Stop")
+{
+foreach ($VeeamService in $VeeamServices)
+{
+Stop-Service $VeeamService
+}
+}
+elseif ($StopOrStart -eq "Start")
+{
+foreach ($VeeamService in $VeeamServices)
+{
+Start-Service $VeeamService
+
+}
+}
