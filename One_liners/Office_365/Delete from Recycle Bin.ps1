47e8c5c4ccf8f9615a90bea49f091f35d403af4c
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2021-06-23 15:41:22 +0100

    Update Delete from Recycle Bin.ps1

--- One_liners/Office_365/Delete from Recycle Bin.ps1
+++ One_liners/Office_365/Delete from Recycle Bin.ps1
@@ -1,2 +1,8 @@
+# Info
+#
 #This script removes the user from the recycle bin
+#
+# Version 1.0
+# Written by Jay
+
 Remove-MsolUser -UserPrincipalName ‘User@email.onmicrosoft.com’ -RemoveFromRecycleBin
