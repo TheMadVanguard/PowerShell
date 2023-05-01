04756a15b257efda091358acab7731c609840711
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2023-05-01 18:50:05 +0100

    Update AD_Deployment.ps1
    Added the install commands

--- Scripts/Work_In_Progress/AD_Deployment.ps1
+++ Scripts/Work_In_Progress/AD_Deployment.ps1
@@ -5,13 +5,20 @@
 # Version 0.2
 # Written by Jay
 
+# To install the Active Directory Domain Services (ADDS) role. To do it, run the following command in the PowerShell console:
+
+Install-WindowsFeature AD-Domain-Services –IncludeManagementTools -Verbose
+
+#Make sure that the AD-Domain-Services role is installed:
+
+Get-WindowsFeature -Name *AD*
 
 #Variables
 $DomainName = "DOMAIN.NAME"
 $NetBIOSName = "DOMAINNAME"
 $Password = Read-Host -Prompt 'Enter SafeMode Admin Password' -AsSecureString
 
-# Config
+#Config
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
